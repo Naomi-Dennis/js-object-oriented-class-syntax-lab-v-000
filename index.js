398f9a3b4c303817f81c5a4fabb037ab3523ebcd
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 16:14:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ class BoardMember{
   }
 
   function releasePressStatement(){
-
+    return "You will see great things from Scuber."
   }
   function sayHello(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
