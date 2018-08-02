38934270f7f3944f7fea9737348c2e595eb5631b
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 16:16:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class BoardMember{
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
  sayHi(){
-    this.sayHello()
+    sayHello()
   }
 
 }
