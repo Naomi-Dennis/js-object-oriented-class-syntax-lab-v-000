aeaf9c4b5b65e866f99d2a790e2002ee9cb97aa6
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 16:12:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 class BoardMember{
-  constructor(){
-    
+  constructor(name, homeState, training){
+      this.name = name
+      this.homeState = homeState
+      this.training = training
+  }
+
+  function veto(){
+
+  }
+
+  function approve(){
+
+  }
+
+  function doCharity(){
+
+  }
+
+  function releasePressStatement(){
+
+  }
+  function sayHi(){
+
+  }
+}
+
+class Ceo extends BoardMember{
+  constructor(name, homeState, training){
+    super(name, homeState, training)
   }
 }
